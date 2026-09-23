532bce25fca0b3aa3be1ccba0afa35736a07cdf3
andresgafer2018-genius <andresgafer2018@gmail.com>
2026-09-23 12:25:59 -0300

    Add files via upload

--- app/api/email/bienvenida/route.ts
+++ app/api/email/bienvenida/route.ts
@@ -12,7 +12,7 @@ export async function POST(req: NextRequest) {
     }
 
     const { data, error } = await resend.emails.send({
-      from: 'Puntual <horarios@puntualhorarios.com>',
+      from: 'Puntual <horarios@send.puntualhorarios.com>',
       replyTo: 'puntualhorarios@gmail.com',
       to: email,
       subject: `¡Bienvenido/a a Puntual, ${nombre}!`,
